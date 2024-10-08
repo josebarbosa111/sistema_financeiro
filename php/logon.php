ffe65f743e5616b3e368bf6e2d5030fbc97b16cf
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-07 21:41:03 -0300

    Tela de Login com banc de dados

---
+++ php/logon.php
@@ -0,0 +1,27 @@
+<?php
+//recebendo os dados da tela de login
+
+$email = $_POST['email'];
+$password = $_POST['senha'];
+
+//link da conexão
+include 'conexao.php';
+
+//Puxar os dados do banco
+
+$sql = "SELECT * FROM tb_user WHERE email = '$email'";
+$query = $conexao->query($sql);
+$resultado = $query->fetch_assoc();
+
+
+$email_banco = $resultado['email'];
+$senha_banco  = $resultado['senha'];
+
+
+if ($email == $email_banco &&  $password == $senha_banco) {
+    header('location: ../tela_inicial.html');
+}else {
+    echo "<script> alert('Usuario ou senha Invalida'); history.back(); </script>"; 
+}
+
+?>
